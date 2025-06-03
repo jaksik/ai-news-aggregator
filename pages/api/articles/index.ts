13a472d7e3b332e21c79cdbb8a194589709a3c3f
jaksik <connorjaksik@gmail.com>
2025-06-02 20:13:48 -0500

    created new test dashboard page

--- pages/api/articles/index.ts
+++ pages/api/articles/index.ts
@@ -1,6 +1,8 @@
 // File: pages/api/articles/index.ts
 // Purpose: Serves as the main API for retrieving articles with powerful filtering capabilities. Used by:
 
+
+
 import { createMethodHandler } from '../../../lib/api/routeHandlerFactory';
 import { listArticles } from '../../../lib/api/controllers/articlesController';
 
